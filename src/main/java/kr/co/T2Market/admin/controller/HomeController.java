c5355897a395aee94ad3691d510e3aa3d0910fd2
kimsuhyun <tg816@naver.com>
2022-07-11 12:30:03 +0900

    header, main 통합

--- src/main/java/kr/co/T2Market/admin/controller/HomeController.java
+++ src/main/java/kr/co/T2Market/admin/controller/HomeController.java
@@ -6,7 +6,7 @@ import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -14,10 +14,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
-@org.springframework.stereotype.Controller
-public class Controller {
+@Controller
+public class HomeController {
 	
-	private static final Logger logger = LoggerFactory.getLogger(Controller.class);
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
 	/**
 	 * Simply selects the home view to render by returning its name.
