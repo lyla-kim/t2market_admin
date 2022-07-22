595d7cd0007ae0ea7552bc35b1bc7a7b3f46be24
idle918 <fcxz91@Naver.com>
2022-07-22 10:17:12 +0900

    Merge branch 'master' of https://github.com/lyla-kim/t2market_admin.git
    namgunwoo11111

--- src/main/java/kr/co/T2Market/sales/controller/SalesController.java
+++ src/main/java/kr/co/T2Market/sales/controller/SalesController.java
@@ -1,5 +1,18 @@
 package kr.co.T2Market.sales.controller;
 
-public class SalesController {
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Controller
+@RequestMapping("/sales")
+@Log4j2
+@AllArgsConstructor
+public class SalesController {
+	@GetMapping("/sales")
+	public void sales() {
+	}
 }
